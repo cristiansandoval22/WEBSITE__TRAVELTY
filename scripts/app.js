7b1eb314ce96df7a0cfbf36b5d5b01f259bcddcf
Cristian Sandoval <cristiansandoval2202@gmail.com>
2024-07-03 13:32:08 -0500

    web desing finish

--- scripts/app.js
+++ scripts/app.js
@@ -42,3 +42,11 @@ function scrollHeader(){
     if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
